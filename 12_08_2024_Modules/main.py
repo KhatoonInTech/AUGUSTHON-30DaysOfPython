ab100fd2ac16f7d672f89112e489b855b6a67b39
Ayesha Noreen <154872130+KhatoonInTech@users.noreply.github.com>
2024-08-11 12:42:05 +0500

    AUGUSTHON -Episode 12/30

---
+++ 12_08_2024_Modules/main.py
@@ -0,0 +1,14 @@
+# AUGUSTHON - Day 12 - Modules in Python
+
+from mymodule import generate_full_name as fullname, sum_two_nums as total, person as p, gravity as g
+print(fullname('Ayesha','Noreen'))
+print(total(1, 9))
+mass = 100
+weight = mass * g
+print(weight)
+print(p)
+print(p['firstname'])
+
+print('''
+ #Created by: Ayesha Noreen |Follow us on www.linkedin.com/in/khatoonintech | We love #AUGUSTHON 💕
+      ''')
